57cbc50fc4d97c5aa5e0e3a80c30f5c7e72776d4
Deadpoolmine <2841954631@qq.com>
2023-08-30 00:02:29 -0400

    fix SplitFS multithread test script, renew KILLER's testpoint

--- killer-evaluation/FIG-MT-FIO/test.sh
+++ killer-evaluation/FIG-MT-FIO/test.sh
@@ -5,10 +5,12 @@ ABS_PATH=$(where_is_script "$0")
 TOOLS_PATH=$ABS_PATH/../../tools
 BOOST_DIR=$ABS_PATH/../../../splitfs/splitfs
 
-FILE_SYSTEMS=( "NOVA" "PMFS" "NOVA-RELAX" "KILLER" "SplitFS-FIO" "EXT4-DAX" "XFS-DAX" )
+# FILE_SYSTEMS=( "NOVA" "PMFS" "NOVA-RELAX" "KILLER" "SplitFS-FIO" "EXT4-DAX" "XFS-DAX" )
+FILE_SYSTEMS=( "SplitFS-FIO" )
 FILE_SIZES=( $((1 * 1024)) )
-NUM_JOBS=( 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16)
-TABLE_NAME="$ABS_PATH/performance-comparison-table"
+# NUM_JOBS=( 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16)
+NUM_JOBS=( 1 )
+TABLE_NAME="$ABS_PATH/performance-comparison-table-SplitFS"
 table_create "$TABLE_NAME" "file_system ops job file_size bandwidth(MiB/s)"
 
 loop=1
@@ -36,7 +38,11 @@ do
                     bash "$TOOLS_PATH"/setup.sh "$file_system" "null" "0"
                     export LD_LIBRARY_PATH="$BOOST_DIR"
                     export NVP_TREE_FILE="$BOOST_DIR"/bin/nvp_nvp.tree
-                    BW=$(LD_PRELOAD=$BOOST_DIR/libnvp.so fio -directory=/mnt/pmem0 -fallocate=none -direct=0 -iodepth 1 -thread -numjobs="$job" -rw=write -ioengine=sync -bs="4K" -size="$fsize"M -name=test | grep WRITE: | awk '{print $2}' | sed 's/bw=//g' | "$TOOLS_PATH"/converter/to_MiB_s)
+                    if (( job == 1 )); then
+                        BW=$(LD_PRELOAD=$BOOST_DIR/libnvp.so fio -filename="/mnt/pmem0/test" -fallocate=none -direct=0 -iodepth 1 -rw=write -ioengine=sync -bs="4K" -size="$fsize"M -name=test | grep WRITE: | awk '{print $2}' | sed 's/bw=//g' | "$TOOLS_PATH"/converter/to_MiB_s)
+                    else
+                        BW=$(LD_PRELOAD=$BOOST_DIR/libnvp.so fio -directory=/mnt/pmem0 -fallocate=none -direct=0 -iodepth 1 -thread -numjobs="$job" -rw=write -ioengine=sync -bs="4K" -size="$fsize"M -name=test | grep WRITE: | awk '{print $2}' | sed 's/bw=//g' | "$TOOLS_PATH"/converter/to_MiB_s)
+                    fi
                 else
                     bash "$TOOLS_PATH"/setup.sh "$file_system" "main" "0"
                     BW=$(bash "$TOOLS_PATH"/fio.sh "$fpath" 4K "$fsize" "$job" | grep WRITE: | awk '{print $2}' | sed 's/bw=//g' | "$TOOLS_PATH"/converter/to_MiB_s)
@@ -52,7 +58,11 @@ do
                     bash "$TOOLS_PATH"/setup.sh "$file_system" "null" "0"
                     export LD_LIBRARY_PATH="$BOOST_DIR"
                     export NVP_TREE_FILE="$BOOST_DIR"/bin/nvp_nvp.tree
-                    BW=$(LD_PRELOAD=$BOOST_DIR/libnvp.so fio -directory=/mnt/pmem0 -fallocate=none -direct=0 -iodepth 1 -thread -numjobs="$job" -rw=randwrite -ioengine=sync -bs="4K" -size="$fsize"M -name=test | grep WRITE: | awk '{print $2}' | sed 's/bw=//g' | "$TOOLS_PATH"/converter/to_MiB_s)
+                    if (( job == 1 )); then
+                        BW=$(LD_PRELOAD=$BOOST_DIR/libnvp.so fio -filename="/mnt/pmem0/test" -fallocate=none -direct=0 -iodepth 1 -rw=randwrite -ioengine=sync -bs="4K" -size="$fsize"M -name=test | grep WRITE: | awk '{print $2}' | sed 's/bw=//g' | "$TOOLS_PATH"/converter/to_MiB_s)
+                    else
+                        BW=$(LD_PRELOAD=$BOOST_DIR/libnvp.so fio -directory=/mnt/pmem0 -fallocate=none -direct=0 -iodepth 1 -thread -numjobs="$job" -rw=randwrite -ioengine=sync -bs="4K" -size="$fsize"M -name=test | grep WRITE: | awk '{print $2}' | sed 's/bw=//g' | "$TOOLS_PATH"/converter/to_MiB_s)
+                    fi
                 else
                     bash "$TOOLS_PATH"/setup.sh "$file_system" "main" "0"
                     BW=$(bash "$TOOLS_PATH"/fio-rand.sh $fpath 4K "$fsize" "$job" | grep WRITE: | awk '{print $2}' | sed 's/bw=//g' | "$TOOLS_PATH"/converter/to_MiB_s)
@@ -68,7 +78,11 @@ do
                     bash "$TOOLS_PATH"/setup.sh "$file_system" "null" "0"
                     export LD_LIBRARY_PATH="$BOOST_DIR"
                     export NVP_TREE_FILE="$BOOST_DIR"/bin/nvp_nvp.tree
-                    BW=$(LD_PRELOAD=$BOOST_DIR/libnvp.so fio -directory=/mnt/pmem0 -fallocate=none -direct=0 -iodepth 1 -thread -numjobs="$job" -rw=read -ioengine=sync -bs="4K" -size="$fsize"M -name=test | grep READ: | awk '{print $2}' | sed 's/bw=//g' | "$TOOLS_PATH"/converter/to_MiB_s)
+                    if (( job == 1 )); then
+                        BW=$(LD_PRELOAD=$BOOST_DIR/libnvp.so fio -filename="/mnt/pmem0/test" -fallocate=none -direct=0 -iodepth 1 -rw=read -ioengine=sync -bs="4K" -size="$fsize"M -name=test | grep READ: | awk '{print $2}' | sed 's/bw=//g' | "$TOOLS_PATH"/converter/to_MiB_s)
+                    else
+                        BW=$(LD_PRELOAD=$BOOST_DIR/libnvp.so fio -directory=/mnt/pmem0 -fallocate=none -direct=0 -iodepth 1 -thread -numjobs="$job" -rw=read -ioengine=sync -bs="4K" -size="$fsize"M -name=test | grep READ: | awk '{print $2}' | sed 's/bw=//g' | "$TOOLS_PATH"/converter/to_MiB_s)
+                    fi
                 else
                     bash "$TOOLS_PATH"/setup.sh "$file_system" "main" "0"
                     BW=$(bash "$TOOLS_PATH"/fio.sh "$fpath" 4K "$fsize" "$job" "read" | grep READ: | awk '{print $2}' | sed 's/bw=//g' | "$TOOLS_PATH"/converter/to_MiB_s)
@@ -84,7 +98,11 @@ do
                     bash "$TOOLS_PATH"/setup.sh "$file_system" "null" "0"
                     export LD_LIBRARY_PATH="$BOOST_DIR"
                     export NVP_TREE_FILE="$BOOST_DIR"/bin/nvp_nvp.tree
-                    BW=$(LD_PRELOAD=$BOOST_DIR/libnvp.so fio -directory=/mnt/pmem0 -fallocate=none -direct=0 -iodepth 1 -thread -numjobs="$job" -rw=randread -ioengine=sync -bs="4K" -size="$fsize"M -name=test | grep READ: | awk '{print $2}' | sed 's/bw=//g' | "$TOOLS_PATH"/converter/to_MiB_s)
+                    if (( job == 1 )); then
+                        BW=$(LD_PRELOAD=$BOOST_DIR/libnvp.so fio -filename="/mnt/pmem0/test" -fallocate=none -direct=0 -iodepth 1 -rw=randread -ioengine=sync -bs="4K" -size="$fsize"M -name=test | grep READ: | awk '{print $2}' | sed 's/bw=//g' | "$TOOLS_PATH"/converter/to_MiB_s)    
+                    else
+                        BW=$(LD_PRELOAD=$BOOST_DIR/libnvp.so fio -directory=/mnt/pmem0 -fallocate=none -direct=0 -iodepth 1 -thread -numjobs="$job" -rw=randread -ioengine=sync -bs="4K" -size="$fsize"M -name=test | grep READ: | awk '{print $2}' | sed 's/bw=//g' | "$TOOLS_PATH"/converter/to_MiB_s)
+                    fi
                 else
                     bash "$TOOLS_PATH"/setup.sh "$file_system" "main" "0"
                     BW=$(bash "$TOOLS_PATH"/fio.sh "$fpath" 4K "$fsize" "$job" "randread" | grep READ: | awk '{print $2}' | sed 's/bw=//g' | "$TOOLS_PATH"/converter/to_MiB_s)
