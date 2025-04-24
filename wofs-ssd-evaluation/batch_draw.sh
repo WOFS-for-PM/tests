7c783295317f0b9f8413d558ede51f66f441202f
Deadpoolmine <2841954631@qq.com>
2025-04-24 12:37:55 +0800

    to(ssd-eval): add scripts for one-click test

---
+++ wofs-ssd-evaluation/batch_draw.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+for filename in `ls`
+do
+  if test -d "$filename" ; then
+
+    if ( echo "$filename" | grep -q "Deprecated" ); then 
+      continue
+    fi
+
+    if ( echo "$filename" | grep -q "Finished" ); then 
+      continue
+    fi
+
+    if ( echo "$filename" | grep -q "TODO" ); then 
+      continue
+    fi
+
+    cd "$filename" || exit
+
+    if [ -f "plot.ipynb" ]; then
+        ipython -c "%run plot.ipynb"
+    fi
+
+    if [ -f "plot-breakdown.ipynb" ]; then
+        bash breakdown.sh
+        ipython -c "%run plot-breakdown.ipynb"
+    fi
+    
+    cd - || exit
+  fi
+done
---
+++ wofs-ssd-evaluation/run_all.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+OUTNAME="stdout"
+
+loop=1
+if [ "$1" ]; then
+  loop=$1
+fi
+
+for filename in `ls`
+do
+  if test -d "$filename" ; then
+
+    if ( echo "$filename" | grep -q "Deprecated" ); then 
+      continue
+    fi
+
+    if ( echo "$filename" | grep -q "Finished" ); then 
+      continue
+    fi
+
+    if ( echo "$filename" | grep -q "TODO" ); then 
+      continue
+    fi
+
+    if ( echo "$filename" | grep -q "STAT" ); then 
+      continue
+    fi
+
+    if ( echo "$filename" | grep -q "TABLE-CC" ); then 
+      echo "Manually check $filename in the VM"
+      continue
+    fi
+
+    cd "$filename" || exit
+
+    bash test.sh "$loop" > $OUTNAME
+
+    if [ -f "test-madfs.sh" ]; then
+        bash test-madfs.sh "$loop" >> $OUTNAME
+    fi
+    
+    # Aggregate Results
+    if [ -f "agg.sh" ]; then
+        bash agg.sh "$loop"
+    fi
+
+    if [ -f "table.py" ]; then
+        python3 table.py > latex-table
+        # replace KILLER with WOLVES
+        sed -i 's/KILLER/WOLVES/g' latex-table
+    fi
+
+    cd - || exit
+  fi
+done
+
+# Draw Figures
+bash batch_draw.sh
+# Fetch all figures in fig-fetched
+bash fetch_all_figures.sh
